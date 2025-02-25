65c151c73397fe488b36bd61d47bb302a4f15545
Pov-Yanghai <Yanghai.pov@student.cadt.edu.kh>
2025-02-25 13:45:41 +0700

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "./component/Header"
+import Body from "./component/Body";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Body />
+    </>
+  );
+}
+
+export default App;
